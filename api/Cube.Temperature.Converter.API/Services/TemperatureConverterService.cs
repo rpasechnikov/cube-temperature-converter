51dcaa50eca27ee0e7e31b0a252a18c98792323b
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2021-04-26 18:41:09 +1000

    Added converters in temp converter service

--- api/Cube.Temperature.Converter.API/Services/TemperatureConverterService.cs
+++ api/Cube.Temperature.Converter.API/Services/TemperatureConverterService.cs
@@ -1,4 +1,5 @@
-﻿using Cube.Temperature.Converter.API.Services.Interfaces;
+﻿using Cube.Temperature.Converter.API.Enums;
+using Cube.Temperature.Converter.API.Services.Interfaces;
 using Cube.Temperature.Converter.API.ViewModels;
 using System;
 
@@ -14,7 +15,66 @@ namespace Cube.Temperature.Converter.API.Services
         /// </summary>
         public double Convert(TemperatureConversionViewModel viewModel)
         {
-            throw new NotImplementedException();
+            const string ERROR_FORMAT = 
+                "{0} temperature type is not supported. " +
+                "Only Celcius, Fahrenheit and Kelvin {1} temperature types are supported.";
+
+            double inCelcius;
+
+            switch (viewModel.SourceTemperatureType)
+            {
+                case TemperatureType.Celcius:
+                    inCelcius = viewModel.Value;
+                    break;
+                case TemperatureType.Fahrenheit:
+                    inCelcius = ConvertFahrenheitToCelcius(viewModel.Value);
+                    break;
+                case TemperatureType.Kelvin:
+                    inCelcius = ConvertKelvinToCelcius(viewModel.Value);
+                    break;
+                default:
+                    throw new NotSupportedException(
+                        string.Format(
+                            ERROR_FORMAT,
+                            viewModel.SourceTemperatureType.ToString(),
+                            "input"));
+            }
+
+            switch (viewModel.DestinationTemperatureType)
+            {
+                case TemperatureType.Celcius:
+                    return inCelcius;
+                case TemperatureType.Fahrenheit:
+                    return ConvertCelciusToFahrenheit(inCelcius);
+                case TemperatureType.Kelvin:
+                    return ConvertCelciusToKelvin(inCelcius);
+                default:
+                    throw new NotSupportedException(
+                        string.Format(
+                            ERROR_FORMAT,
+                            viewModel.DestinationTemperatureType.ToString(),
+                            "output"));
+            }
+        }
+
+        private double ConvertCelciusToFahrenheit(double value)
+        {
+            return (value * 9) / 5 + 32;
+        }
+
+        private double ConvertFahrenheitToCelcius(double value)
+        {
+            return 5.0 / 9.0 * (value - 32);
+        }
+
+        private double ConvertCelciusToKelvin(double value)
+        {
+            return value + 273.15;
+        }
+
+        private double ConvertKelvinToCelcius(double value)
+        {
+            return value - 273.15;
         }
     }
 }
